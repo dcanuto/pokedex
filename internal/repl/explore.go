7fdcb0f4eef4371e03add5e2cd314215b6610bc5
dcanuto <danny.canuto@gmail.com>
2026-03-30 09:13:28 -0400

    refactor explore to use client

--- internal/repl/explore.go
+++ internal/repl/explore.go
@@ -1,79 +1,16 @@
 package repl
 
 import (
-	"encoding/json"
 	"fmt"
-
-	"github.com/dcanuto/pokedexcli/internal/pokecache"
 )
 
-type locationArea struct {
-	ID                   int    `json:"id"`
-	Name                 string `json:"name"`
-	GameIndex            int    `json:"game_index"`
-	EncounterMethodRates []struct {
-		EncounterMethod struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"encounter_method"`
-		VersionDetails []struct {
-			Rate    int `json:"rate"`
-			Version struct {
-				Name string `json:"name"`
-				URL  string `json:"url"`
-			} `json:"version"`
-		} `json:"version_details"`
-	} `json:"encounter_method_rates"`
-	Location struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"location"`
-	Names []struct {
-		Name     string `json:"name"`
-		Language struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"language"`
-	} `json:"names"`
-	PokemonEncounters []struct {
-		Pokemon struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"pokemon"`
-		VersionDetails []struct {
-			Version struct {
-				Name string `json:"name"`
-				URL  string `json:"url"`
-			} `json:"version"`
-			MaxChance        int `json:"max_chance"`
-			EncounterDetails []struct {
-				MinLevel        int   `json:"min_level"`
-				MaxLevel        int   `json:"max_level"`
-				ConditionValues []any `json:"condition_values"`
-				Chance          int   `json:"chance"`
-				Method          struct {
-					Name string `json:"name"`
-					URL  string `json:"url"`
-				} `json:"method"`
-			} `json:"encounter_details"`
-		} `json:"version_details"`
-	} `json:"pokemon_encounters"`
-}
-
 func commandExplore(config *config, desiredLocation string) error {
-	url := "https://pokeapi.co/api/v2/location-area/" + desiredLocation
-
-	body, err := pokecache.GetFromOrAddToCache(url, &config.client.Cache)
+	location, err := config.client.GetLocation(desiredLocation)
 	if err != nil {
 		return err
 	}
 
-	m := locationArea{}
-	err = json.Unmarshal(body, &m)
-	if err != nil {
-		return err
-	}
-	for _, encounter := range m.PokemonEncounters {
+	for _, encounter := range location.PokemonEncounters {
 		fmt.Println(encounter.Pokemon.Name)
 	}
 	return nil
